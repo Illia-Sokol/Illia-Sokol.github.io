88cef5147ecaf8fdc02e0395ffa7a435f68fd8a0
Illia Sokol <illiasokolovskyi@gmail.com>
2016-05-22 01:57:11 +0300

    add concat and min css files

--- JS/task_17-18/grunt/gruntfile.js
+++ JS/task_17-18/grunt/gruntfile.js
@@ -6,24 +6,44 @@ module.exports = function(grunt) {
 				separator: ';',
 			},
 			dist: {
-				src: ['src/js/*.js'],
+				src: 'src/js/*.js',
 				dest: 'dist/js/built.js'
 			}
 		},
 		uglify: {
 			my_target: {
 				files: {
-					'dist/js/built.min.js': ['src/js/*.js']
+					'dist/js/built.min.js': ['dist/js/built.js']
 				}
+			}
+		},
+		concat_css: {
+			options: {
+
+			},
+			all: {
+				src: ["src/css/*.css"],
+				dest: "dist/css/main.css"
+			},
+		},
+		cssmin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'dist/css/',
+					src: ['main.css'],
+					dest: 'dist/css/',
+					ext: '.min.css'
+				}]
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-concat-css');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-	grunt.registerTask('default', ['concat', 'uglify']);
+	grunt.registerTask('default', ['concat', 'uglify', 'concat_css', 'cssmin']);
 };
-
-
 
--- JS/task_17-18/gulp/gulpfile.js
+++ JS/task_17-18/gulp/gulpfile.js
@@ -1,22 +1,39 @@
 var gulp = require('gulp'),
 		concat = require('gulp-concat'),
 		rename = require("gulp-rename"),
-		uglify = require('gulp-uglify');
+		uglify = require('gulp-uglify'),
+		uglifycss = require('gulp-uglifycss');
 
 
-	gulp.task('concat', function() {
+	gulp.task('concatJS', function() {
 		return gulp.src('src/js/*.js')
 		.pipe(concat('built.js'))
 		.pipe(gulp.dest('dist/js/'));
 	});
 
-	gulp.task('compress', ['concat'], function() {
+	gulp.task('concatCSS', function() {
+		return gulp.src('src/css/*.css')
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest('dist/css/'));
+	});
+
+	gulp.task('compress', ['concatJS'], function() {
   	return gulp.src('dist/js/*.js')
     .pipe(uglify())
      .pipe(rename('built.min.js'))
     .pipe(gulp.dest('dist/js/'));
 	});
 
-	gulp.task('default', ['compress'], function() {
+	gulp.task('conmpressCSS', ['concatCSS'], function () {
+	gulp.src('src/css/*.css')
+	.pipe(uglifycss({
+		"maxLineLen": 80,
+		"uglyComments": true
+	}))
+	.pipe(rename('main.min.css'))
+	.pipe(gulp.dest('dist/css/'));
+});
+
+	gulp.task('default', ['compress', 'conmpressCSS'], function() {
   console.log("Yess!")
 })
